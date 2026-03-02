909b2010a0b905816f3a9ea65a12e977525a89d2
Jumper26z <1819963195@qq.com>
2026-03-02 13:12:59 +0800

    Delete Graph_Unlearn directory

--- Graph_Unlearn/config.py
+++
@@ -1,7 +0,0 @@
-RAW_DATA_PATH = 'temp_data/raw_data/'
-PROCESSED_DATA_PATH = 'temp_data/processed_data/'
-MODEL_PATH = 'temp_data/models/'
-ANALYSIS_PATH = 'temp_data/analysis_data/'#分析结果目录
-
-# database name
-DATABASE_NAME = "unlearning_gnn"
